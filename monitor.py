727d01abf6152a6b38c859b36cf8c5198bda8840
dindi-cmd <dindigalaakhil@gmail.com>
2026-03-25 12:32:17 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,6 +4,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 options = Options()
 options.add_argument("--headless")
